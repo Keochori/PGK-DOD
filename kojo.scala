77284d08ef511a21fe4c23a1ee81b396606c52ba
Matthew <69148826+Keochori@users.noreply.github.com>
2025-09-17 01:06:20 +0200

    Delete kojo.scala

--- kojo.scala
+++
@@ -1,6 +0,0 @@
-//> using scala 3
-//> using dep "net.kogics:kojo-lib:0.3.1,url=https://github.com/litan/kojo-lib/releases/download/v0.3.1/kojo-lib-assembly-0.3.1.jar"
-
-export net.kogics.kojo.Swedish.*, padda.*, CanvasAPI.*, TurtleAPI.*
-export builtins.activateCanvas
-export java.awt.Color
